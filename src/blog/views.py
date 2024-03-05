6b21fd85d5d31f94718218ee48329ec7f042a679
onepiece <alialemi.droid21@gmail.com>
2024-03-05 08:07:04 +0330

    #2

--- src/blog/views.py
+++ src/blog/views.py
@@ -3,22 +3,22 @@ from django.http import HttpResponse
 
 posts = [
     {
-        'author': 'ali',
-        'title': 'django',
-        'content': 'im power source',
-        'date_post': '2020 / 12 / 20',
+    'author': 'ali',
+    'title': 'django',
+    'content': 'im power source',
+    'date_post': '2020  12  20',
     },
     {
     'author': 'mohammad',
     'title': 'reactpy',
     'content': 'im end power source',
-    'date_post': '2022 / 5 / 00',
+    'date_post': '2022  5  00',
     },
     {
     'author': 'ahmad',
     'title': 'python',
     'content': 'im start power source',
-    'date_post': '2018 / 10 / 30',
+    'date_post': '2018  10  30',
     },
 ]
 
@@ -27,8 +27,8 @@ def Home(request):
     context={
         'posts': posts
     }
-    return render(request, 'blog/home.html')
+    return render(request, 'blog/home.html', context)
 
 def About(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': About})
 # end views
