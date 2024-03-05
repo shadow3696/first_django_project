4760a7f7001dfb12c68a75005eaa91403439d44d
onepiece <alialemi.droid21@gmail.com>
2024-03-05 08:03:42 +0330

    #2

--- src/blog/views.py
+++ src/blog/views.py
@@ -8,16 +8,25 @@ posts = [
         'content': 'im power source',
         'date_post': '2020 / 12 / 20',
     },
-        {
-        'author': 'ali',
-        'title': 'django',
-        'content': 'im power source',
-        'date_post': '2020 / 12 / 20',
+    {
+    'author': 'mohammad',
+    'title': 'reactpy',
+    'content': 'im end power source',
+    'date_post': '2022 / 5 / 00',
+    },
+    {
+    'author': 'ahmad',
+    'title': 'python',
+    'content': 'im start power source',
+    'date_post': '2018 / 10 / 30',
     },
 ]
 
 # start views  # inja ma omadim func haiee ro tarif kardim ke ba ejra shodan chizi ro be namayesh mizaran
 def Home(request):
+    context={
+        'posts': posts
+    }
     return render(request, 'blog/home.html')
 
 def About(request):
